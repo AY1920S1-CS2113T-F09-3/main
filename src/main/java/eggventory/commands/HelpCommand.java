48762ad9df8ab7a444e761db0a4a1bd1a6964446
yanprosobo <peiyanbo@hotmail.com>
2019-10-17 02:18:02 +0800

    Skeleton code for HelpCommand class

---
+++ src/main/java/eggventory/commands/HelpCommand.java
@@ -0,0 +1,17 @@
+package eggventory.commands;
+
+import eggventory.Ui;
+import eggventory.Storage;
+import eggventory.StockList;
+import eggventory.enums.CommandType;
+import eggventory.commands.Command;
+
+public class HelpCommand extends Command {
+    private CommandType type;
+    private String options;
+
+    public HelpCommand(CommandType type, String options) {
+        this.type = type;
+        this.options = options;
+    }
+}
