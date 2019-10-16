99b1ec8f18cff8a32992849dd890495240d09ca4
Deculsion <danasanma@gmail.com>
2019-10-16 18:22:24 +0800

    Style fixes

--- src/main/java/eggventory/commands/add/AddStockTypeCommand.java
+++ src/main/java/eggventory/commands/add/AddStockTypeCommand.java
@@ -8,35 +8,34 @@ import eggventory.enums.CommandType;
 import eggventory.exceptions.BadInputException;
 
 public class AddStockTypeCommand extends Command {
-   private String name;
-   private CommandType type;
+    private String name;
+    private CommandType type;
 
-   /**
-    * Creates a new StockType in a StorageList
-    * @param type The type of command this is.
-    * @param name Name of StockType to add.
-    */
-   public AddStockTypeCommand(CommandType type, String name) {
-      this.name = name;
-      this.type = type;
-   }
+    /**
+     * Creates a new StockType in a StorageList.
+     * @param type The type of command this is.
+     * @param name Name of StockType to add.
+     */
+    public AddStockTypeCommand(CommandType type, String name) {
+        this.name = name;
+        this.type = type;
+    }
 
-   /**
-    * Executes actual adding of StockType.
-    * @param list StockType to add the item to.
-    * @param ui Ui object to display output to.
-    * @param storage Storage object to handle saving and loading of any data.
-    * @throws BadInputException
-    */
-   @Override
-   public String execute(StockList list, Ui ui, Storage storage) {
-      String output;
+    /**
+     * Executes actual adding of StockType.
+     * @param list StockType to add the item to.
+     * @param ui Ui object to display output to.
+     * @param storage Storage object to handle saving and loading of any data.
+     */
+    @Override
+    public String execute(StockList list, Ui ui, Storage storage) {
+        String output;
 
-      list.addStockType(name);
-      output = String.format("Nice! I have successfully added the stocktype: %s", name);
+        list.addStockType(name);
+        output = String.format("Nice! I have successfully added the stocktype: %s", name);
 
-      ui.print(output);
+        ui.print(output);
 
-      return output;
-   }
+        return output;
+    }
 }
--- src/test/java/eggventory/commands/add/AddStockTypeCommandTest.java
+++ src/test/java/eggventory/commands/add/AddStockTypeCommandTest.java
@@ -3,12 +3,10 @@ package eggventory.commands.add;
 import eggventory.StockList;
 import eggventory.Storage;
 import eggventory.Ui;
-import eggventory.commands.AddCommand;
 import eggventory.enums.CommandType;
-import eggventory.exceptions.BadInputException;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AddStockTypeCommandTest {
 
@@ -21,7 +19,8 @@ class AddStockTypeCommandTest {
         StockList testList = new StockList();
         testList.addStockType("testStockType");
 
-        String output = new AddStockTypeCommand(CommandType.ADD, "testStockType").execute(testList, testUi, testStorage);
+        String output = new AddStockTypeCommand(CommandType.ADD, "testStockType")
+                .execute(testList, testUi, testStorage);
 
         assertEquals(String.format("Nice! I have successfully added the stocktype: testStockType"), output);
     }
