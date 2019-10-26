6fb883be838c505d1468c72de541e5b1d667b61c
Deculsion <danasanma@gmail.com>
2019-10-26 14:54:07 +0800

    Add StockListTest

---
+++ src/test/java/eggventory/StockListTest.java
@@ -0,0 +1,44 @@
+package eggventory;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StockListTest {
+    //@@author Deculsion
+    @Test
+    void addStockType() {
+    }
+
+    @Test
+    void deleteStockType() {
+    }
+
+    @Test
+    void getStockType() {
+    }
+
+    @Test
+    void getStockTypeQuantity() {
+    }
+
+    @Test
+    void addStock() {
+    }
+
+    @Test
+    void deleteStock() {
+    }
+
+    @Test
+    void setStock() {
+    }
+
+    @Test
+    void setStockType() {
+    }
+
+    @Test
+    void getStockQuantity() {
+    }
+}
