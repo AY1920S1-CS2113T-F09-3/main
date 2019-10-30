3d1168c6207c3f244e5e5319ad05c0ff2243b4b1
Rishab Patwari <e0311251@u.nus.edu>
2019-10-30 22:11:23 +0800

    - Added new DuplicateEntryException which extends from BadInputException - Added test code for EditStockTypeCommand

--- src/test/java/eggventory/logic/commands/edit/EditStockTypeCommandTest.java
+++ src/test/java/eggventory/logic/commands/edit/EditStockTypeCommandTest.java
@@ -1,7 +1,32 @@
 package eggventory.logic.commands.edit;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import eggventory.commons.enums.CommandType;
+import eggventory.commons.exceptions.DuplicateEntryException;
+import eggventory.model.StockList;
+import eggventory.storage.Storage;
+import eggventory.stubs.UiStub;
+import eggventory.ui.Ui;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class EditStockTypeCommandTest {
+    StockList testStockList = new StockList();
+    Ui testUi = new UiStub();
+    Storage testStorage = new Storage("", "");
+
+    @Test
+    public void testExecuteEditStockType_RepeatedStockType_ThrowsDuplicateEntryException() {
+        String testNameA = "TestType1";
+        String testNameB = "TestType2";
+        testStockList.addStockType(testNameA);
+        testStockList.addStockType(testNameB);
+        assertTrue(testStockList.isExistingStockType(testNameA));
+        assertTrue(testStockList.isExistingStockType(testNameB));
+        EditStockTypeCommand cmd = new EditStockTypeCommand(CommandType.EDIT,
+                testNameB, testNameA);
+        assertThrows(DuplicateEntryException.class,() -> cmd.execute(testStockList, testUi, testStorage));
+    }
 
 }
