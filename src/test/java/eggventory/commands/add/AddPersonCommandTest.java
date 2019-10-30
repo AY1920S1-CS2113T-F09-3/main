650a45c01d9a82f64da10e873a42c3954ae78e85
Deculsion <danasanma@gmail.com>
2019-10-30 15:45:24 +0800

    Add add person testing

---
+++ src/test/java/eggventory/commands/add/AddPersonCommandTest.java
@@ -0,0 +1,42 @@
+package eggventory.commands.add;
+
+import eggventory.commons.enums.CommandType;
+import eggventory.commons.exceptions.BadInputException;
+import eggventory.logic.commands.add.AddPersonCommand;
+import eggventory.model.PersonList;
+import eggventory.model.StockList;
+import eggventory.storage.Storage;
+import eggventory.ui.Cli;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class AddPersonCommandTest {
+
+    private StockList testStockList = new StockList();
+    private Cli testCli = new Cli();
+    private Storage testStorage = new Storage("");
+    private PersonList PersonList = new PersonList();
+
+    @Test
+    void testExecuteAddPerson_ValidPerson_Succeeds() throws BadInputException {
+        String output = new AddPersonCommand(CommandType.ADD, "A12345A", "Akshay Narayan")
+                .execute(testStockList, testCli, testStorage);
+
+        assertEquals("Nice, I have added this person for you.\n" +
+                "Matriculation No.: A12345A | Name: Akshay Narayan", output);
+
+    }
+
+    @Test
+    void testExecuteAddPerson_RepeatedPerson_ThrowsBadInputException() throws BadInputException {
+        Exception exception = assertThrows(BadInputException.class, () ->
+                new AddPersonCommand(CommandType.ADD, "A12345A", "Akshay Narayan")
+                        .execute(testStockList,testCli,testStorage)
+        );
+
+        assertEquals("The Person with the specified matriculation number already exists!\n Did you"
+                + " mean to edit this Person's details instead?", exception.getMessage());
+    }
+}
