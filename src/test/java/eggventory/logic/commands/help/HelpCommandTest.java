e1489b25c9b52b0bf9db9a49a3815603e46ec417
yanprosobo <peiyanbo@hotmail.com>
2019-11-09 21:44:03 +0800

    Add simple help test command

--- src/test/java/eggventory/logic/commands/help/HelpCommandTest.java
+++ src/test/java/eggventory/logic/commands/help/HelpCommandTest.java
@@ -1,7 +1,26 @@
-package eggventory.logic;
+package eggventory.logic.commands;
+
+import eggventory.stubs.StorageStub;
+import eggventory.stubs.UiStub;
+import eggventory.storage.Storage;
+import eggventory.ui.Ui;
+import eggventory.model.StockList;
+import eggventory.commons.enums.CommandType;
+import eggventory.commons.exceptions.BadInputException;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class HelpCommandTest {
+    StockList testStockList = new StockList();
+    Ui testCli = new UiStub();
+    Storage testStorage = new StorageStub();
 
+    @Test
+    public void testExecuteHelpCommand_InvalidCommand_ThrowsBadInputException() throws BadInputException {
+        assertThrows(BadInputException.class, () -> new HelpCommand(CommandType.HELP, "invalid")
+                .execute(testStockList,testCli,testStorage));
+    }
 }
