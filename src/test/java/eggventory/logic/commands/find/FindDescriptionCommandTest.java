ece04b5c8bcfdc02bb6982842d0b815a467e3ab5
yanprosobo <peiyanbo@hotmail.com>
2019-11-09 19:51:14 +0800

    Fix checkstyle

--- src/test/java/eggventory/logic/commands/find/FindDescriptionCommandTest.java
+++ src/test/java/eggventory/logic/commands/find/FindDescriptionCommandTest.java
@@ -13,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 //@@author yanprosobo
-class FindCommandTest {
+class FindDescriptionCommandTest {
     StockList testStockList = new StockList();
     Ui testCli = new UiStub();
     Storage testStorage = new StorageStub();
@@ -23,22 +23,25 @@ class FindCommandTest {
     public void testExecuteFindDescription_EmptyResult() {
         String search = "search";
         String expected = "Sorry, I could not find any stock containing the description \""
-                + search + "\".\nPlease try a different search string.";
-        String output = new FindDescriptionCommand(CommandType.FIND, search).execute(testStockList, testCli, testStorage);
+                        + search + "\".\nPlease try a different search string.";
+        String output = new FindDescriptionCommand(CommandType.FIND, search)
+                .execute(testStockList, testCli, testStorage);
         assertEquals(expected, output);
     }
 
     //Integration test with add stocktype and add stock. Using "search" as the word to be searching for.
     @Test
     public void testExecuteFindDescription_Success() throws BadInputException {
+        String search = "search";
         testStockList.addStockType("TestType");
         testStockList.addStock("TestType", "#T", 1, "Test search");
         testStockList.addStock("TestType", "#T2", 10, "Test query");
         testStockList.addStock("TestType", "#T3", 100, "Test search query");
 
-        String expected = "1. TestType | #T | 1 | Test search\n" +
-                          "2. TestType | #T3 | 100 | Test search query\n";
-        String output = new FindDescriptionCommand(CommandType.FIND, "search").execute(testStockList, testCli, testStorage);
+        String expected = "1. TestType | #T | 1 | Test search\n"
+                        + "2. TestType | #T3 | 100 | Test search query\n";
+        String output = new FindDescriptionCommand(CommandType.FIND, search)
+                .execute(testStockList, testCli, testStorage);
         assertEquals(expected, output);
     }
 
