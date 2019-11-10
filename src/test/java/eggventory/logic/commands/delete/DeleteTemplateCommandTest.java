abf9fd3b728b4c14496bc928a077648411a341fc
Deculsion <danasanma@gmail.com>
2019-11-10 18:01:23 +0800

    oops

--- src/test/java/eggventory/logic/commands/delete/DeleteTemplateCommandTest.java
+++ src/test/java/eggventory/logic/commands/delete/DeleteTemplateCommandTest.java
@@ -1,8 +1,13 @@
 package eggventory.logic.commands.delete;
 
 import eggventory.commons.enums.CommandType;
+import eggventory.model.StockList;
 import eggventory.model.TemplateList;
 import eggventory.model.loans.Loan;
+import eggventory.storage.Storage;
+import eggventory.stubs.StorageStub;
+import eggventory.stubs.UiStub;
+import eggventory.ui.Ui;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,6 +16,10 @@ class DeleteTemplateCommandTest {
 
     private Loan[] loans = new Loan[1];
     private String testTemplateName = "Test Template";
+
+    private StockList testStockList = new StockList();
+    private Ui testCli = new UiStub();
+    private Storage testStorage = new StorageStub();
 
     DeleteTemplateCommandTest() {
         loans[0] = new Loan("TestCode", "1");
