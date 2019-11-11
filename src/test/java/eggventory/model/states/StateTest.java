43ccf3b72adc272eed5596ad1ac9bf63d6bfd8c4
Rishab Patwari <e0311251@u.nus.edu>
2019-11-11 19:01:26 +0800

    GAVE UP ON MOCKITO

---
+++ src/test/java/eggventory/model/states/StateTest.java
@@ -0,0 +1,16 @@
+package eggventory.model.states;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StateTest {
+
+    @Test
+    void setAllStates() {
+    }
+
+    @Test
+    void clearAllStates() {
+    }
+}
