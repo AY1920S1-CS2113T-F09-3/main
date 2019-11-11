0467aa8138b1674f591800e6d4265c8f27b7d3ec
Deculsion <danasanma@gmail.com>
2019-11-11 16:06:21 +0800

    Add loan testing

--- src/test/java/eggventory/model/loans/LoanTest.java
+++ src/test/java/eggventory/model/loans/LoanTest.java
@@ -1,6 +1,9 @@
 package eggventory.model.loans;
 
+import eggventory.commons.exceptions.BadInputException;
+import eggventory.model.PersonList;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -8,17 +11,27 @@ import java.util.ArrayList;
 //@@author cyanoei
 public class LoanTest {
 
-    Loan loan = new Loan("A1", "R5", 10);
+    private Loan loan;
 
-    private ArrayList<String> data = new ArrayList<>();
+    private ArrayList<String> data;
+
+    @BeforeEach
+    void resetLoanObject() {
+        loan = new Loan("A1", "R5", 10);
+    }
+
+    @BeforeEach
+    void resetDataArray() {
+        data = new ArrayList<>();
+    }
 
     @Test
-    void testLoanEquals_IsEqual_ReturnsTrue() {
+    void loanEquals_IsEqual_ReturnsTrue() {
         Assertions.assertTrue(loan.loanEquals("A1", "R5"));
     }
 
     @Test
-    void testLoanEquals_NotEqual_ReturnsFalse() {
+    void loanEquals_NotEqual_ReturnsFalse() {
         Assertions.assertTrue(loan.loanEquals("A1", "R5"));
         Assertions.assertFalse(loan.loanEquals("not", "R5"));
 
@@ -31,8 +44,6 @@ public class LoanTest {
 
     @Test
     void settersGetters_Exists_ReturnsValue() {
-        loan = new Loan("A1", "R5", 10);
-
         Assertions.assertEquals("A1", loan.getMatricNo());
         Assertions.assertEquals("R5", loan.getStockCode());
 
@@ -43,8 +54,22 @@ public class LoanTest {
     }
 
     @Test
-    void testGetDataAsArray_Input() {
+    void getStockDataAsArray_StockInfoExists_ReturnsSuccess() {
+        data.add("R5");
+        data.add("10");
+
+        Assertions.assertEquals(data, loan.getStockDataAsArray());
+    }
+
+    @Test
+    void getDataAsArray_InputValid_ReturnsSuccess() throws BadInputException {
         data.add("A1");
+        data.add("TestName");
+        data.add("R5");
+        data.add("10");
+        PersonList.add("A1", "TestName");
+
+        Assertions.assertEquals(data, loan.getDataAsArray());
     }
 
 }
