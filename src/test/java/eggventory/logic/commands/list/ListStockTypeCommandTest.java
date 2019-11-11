697b28a37ed436d3c0b72962d07af246586566b8
yanprosobo <peiyanbo@hotmail.com>
2019-11-11 13:30:06 +0800

    Add further test to liststocktypecommand

--- src/test/java/eggventory/logic/commands/list/ListStockTypeCommandTest.java
+++ src/test/java/eggventory/logic/commands/list/ListStockTypeCommandTest.java
@@ -21,15 +21,53 @@ class ListStockTypeCommandTest {
     private Storage testStorage = new StorageStub();
 
     @Test
-    void execute_InvalidStocktype_ThrowsBadInputException() {
-        String invalid = "Invalidtype";
-        String valid = "Validtype";
-        String alsoValid = "AlsoValidType";
+    void execute_InvalidStocktype_ThrowsBadInputException() throws BadInputException{
+        String invalid = "InvalidType";
+        String valid = "ValidType";
 
         testStockList.addStockType(valid);
-        testStockList.addStockType(alsoValid);
-        assertDoesNotThrow(() -> new ListStockTypeCommand(CommandType.LIST, valid).execute(testStockList, testCli, testStorage));
+
+        assertDoesNotThrow(() -> new ListStockTypeCommand(CommandType.LIST, valid)
+                .execute(testStockList, testCli, testStorage));
         assertThrows(BadInputException.class, () -> new ListStockTypeCommand(CommandType.LIST, invalid)
                 .execute(testStockList, testCli, testStorage));
+    }
+
+    @Test
+    void execute_ValidStockTypeWithStocks_Success() throws BadInputException{
+        String validWithStocks = "ValidType";
+        String validWithoutStocks = "AlsoValidType";
+
+        testStockList.addStockType(validWithStocks);
+        testStockList.addStockType(validWithoutStocks);
+        testStockList.addStock(validWithStocks, "#Test1", 1, "Test description");
+
+        assertDoesNotThrow(() -> new ListStockTypeCommand(CommandType.LIST, validWithStocks)
+                .execute(testStockList, testCli, testStorage));
+        assertDoesNotThrow(() -> new ListStockTypeCommand(CommandType.LIST, validWithoutStocks)
+                .execute(testStockList, testCli, testStorage));
+        String output = new ListStockTypeCommand(CommandType.LIST, validWithStocks)
+                .execute(testStockList, testCli, testStorage);
+        String expected = validWithStocks
+                + " INVENTORY\n"
+                + "------------------------\n"
+                + "1. " + validWithStocks + " | #Test1 | 1 | Test description\n"
+                + "\n";
+        assertEquals(expected, output);
+    }
+
+    @Test
+    void execute_ValidZeroQuantiyStocktype_Success() throws BadInputException {
+        String zeroQuantityStockType = "Zero";
+        String someQuantityStockType = "Some";
+
+        testStockList.addStockType(zeroQuantityStockType);
+        testStockList.addStockType(someQuantityStockType);
+        testStockList.addStock(someQuantityStockType, "#Test1", 1, "Test description");
+
+        String output = new ListStockTypeCommand(CommandType.LIST, zeroQuantityStockType)
+                .execute(testStockList, testCli, testStorage);
+        String expected = "There is currently 0 stock with that stocktype.";
+        assertEquals(expected, output);
     }
 }
